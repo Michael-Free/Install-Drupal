0fa9bfd7c386255247386262adfd16278ac6a1b5
Michael Free <michael.d.free@gmail.com>
2023-04-06 20:09:55 -0400

    password input comments added to migrate script. will need to put into other scripts as well

--- migrate-9to10/migrate-9to10.sh
+++ migrate-9to10/migrate-9to10.sh
@@ -5,6 +5,21 @@ DRUPAL9_BACKUP=drupal9_backup.zip
 DRUPAL10_ROOT=/var/www/mydomain.com
 DRUPAL10_SQL_PASS=random_password
 
+# unset password
+# prompt="Enter Password:"
+# while IFS= read -p "$prompt" -r -s -n 1 char
+# do
+#     if [[ $char == $'\0' ]]
+#     then
+#         break
+#     fi
+#     prompt='*'
+#     password+="$char"
+# done
+# echo
+# echo "Done. Password=$password"
+
+
 check_output () {
     echo "placeholder"
 }
